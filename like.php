e9880359f8a5445e87d50ebc59eea299f2d539f8
Sam Headland <1heads65@solent.ac.uk>
2017-05-12 15:45:56 +0100

    update

--- like.php
+++ like.php
@@ -1,15 +1,18 @@
 <?php
 $id = $_GET['id'];
 
-// If the
+// Selects id from post table where the id is equal to the one passed through in the link
 $query = mysql_query("SELECT * FROM post WHERE id='$id'");
 mysql_close($conn);
 
+// If statement to check if query has been made
 if(mysql_num_rows($query)>=1)
 {
 
 }
 			include 'connect.php';
+
+			//Updates post table by adding 1 like to the 'likes' column where the id is equal.
 			mysql_query("UPDATE post SET likes = likes + 1 WHERE id='$id'");
 			mysql_close($conn);
 			echo "<meta http-equiv=\"refresh\" content=\"0; url=home.php\">";
